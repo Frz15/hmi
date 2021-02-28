f93234316ea5cb9e357db73340967e8bc03b5aa2
Frz15 <18802239988@163.com>
2021-02-28 10:56:31 +0800

    Init

---
+++ mini-auto-drivers-master/lidar/point_convert_hmi/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sunyafei/001_code/014_xiaopengG1/zzz/src/tools/point_convert/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-10.0/lib64:/opt/ros/melodic/lib:/opt/ros/melodic/lib"
+export PATH="/opt/ros/melodic/bin:/home/sunyafei/.local/share/umake/bin:/home/sunyafei/.local/bin:/home/sunyafei/.local/bin/:/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/NsightCompute-1.0:/home/sunyafei/.local/share/umake/bin:/home/sunyafei/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/opt/ros/melodic/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export PWD="/home/sunyafei/001_code/014_xiaopengG1/zzz/src/tools/point_convert/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/sunyafei/001_code/014_xiaopengG1/zzz/src/tools/point_convert/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/sunyafei/001_code/014_xiaopengG1/zzz/src/tools/point_convert:$ROS_PACKAGE_PATH"
